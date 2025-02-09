dd083139567329f6b1ada364145946396d49e952
Alexey Bosalyga <alexey.bos@gmail.com>
2025-02-09 22:09:24 +0400

    Add files via upload

---
+++ Vertex.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Vertex
+{
+    public int Value;
+    public Vertex(int val)
+    {
+        Value = val;
+    }
+}
+
+class SimpleGraph
+{
+    Vertex [] vertex;
+    int [][] m_adjacency;
+    int max_vertex;
+
+    public SimpleGraph(int size)
+    {
+        max_vertex = size;
+        m_adjacency = new int [size][size];
+        vertex = new Vertex[size];
+    }
+
+    public void AddVertex(int value)
+    {
+        int emptySlot = 0;
+        for (; vertex[emptySlot] != null; emptySlot++) {
+        }
+        vertex[emptySlot] = new Vertex(value);
+    }
+
+    public void RemoveVertex(int v)
+    {
+        vertex[v] = null;
+        for (int i = 0; i < max_vertex; i++) {
+            m_adjacency[v][i] = 0;
+            m_adjacency[i][v] = 0;
+        }
+    }
+
+    public boolean IsEdge(int v1, int v2)
+    {
+        return m_adjacency[v1][v2] == 1;
+    }
+
+    public void AddEdge(int v1, int v2)
+    {
+        m_adjacency[v1][v2] = 1;
+        m_adjacency[v2][v1] = 1;
+    }
+
+    public void RemoveEdge(int v1, int v2)
+    {
+        m_adjacency[v1][v2] = 0;
+        m_adjacency[v2][v1] = 0;
+    }
+}
+
+
+
+
