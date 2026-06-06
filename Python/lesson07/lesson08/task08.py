4b2c0f27be877495c7f6997ef65a6895bff191f8
Alexey Bosalyga <alexey.bos@gmail.com>
2026-06-06 20:38:57 +0400

    Add files via upload

---
+++ Python/lesson07/lesson08/task08.py
@@ -0,0 +1,39 @@
+
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        for i in range(0, self.max_vertex):
+            if self.vertex[i] is None:
+                self.vertex[i] = Vertex(v)
+                return
+
+    def RemoveVertex(self, v):
+        self.vertex[v] = None
+        for i in range(0, self.max_vertex):
+            self.m_adjacency[i][v] = 0
+            self.m_adjacency[v][i] = 0
+
+    def IsEdge(self, v1, v2):
+        return self.m_adjacency[v1][v2] == 1 or self.m_adjacency[v2][v1] == 1
+
+    def AddEdge(self, v1, v2):
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1, v2):
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
+
+
+
